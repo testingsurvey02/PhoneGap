1bb87051a3e242ee0db1f988e49f3a2c5389d461
testingsurvey02 <testingsurvey02@gmail.com>
2016-11-28 14:04:55 +0530

    Table creation if not exist table

--- TailR.testing/www/js/index.js
+++ TailR.testing/www/js/index.js
@@ -2023,7 +2023,7 @@ function errorCBCustomerListDB(err) {
 					console.log(galleryArr.length);
 					if(galleryArr.length>0){
 						
-						var $galleryImagesList=$prodSelDetailsDiv.find('.gallery-images-list');
+						var $galleryImagesList=$prodSelDetailsDiv.find('ol .gallery-images-list');
 						$galleryImagesList.find('li').remove();
 						
 						jQuery.each(galleryArr, function(indexGal, valueGal){
